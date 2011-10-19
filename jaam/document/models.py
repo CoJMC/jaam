21c4cd93c197ce46bd941faefa99162723fa5f7b
Cole Mickens <cole.mickens@gmail.com>
2011-10-19 10:03:10 -0500

    Added stuff for DocumentCloud config

--- jaam/document/models.py
+++ jaam/document/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from jaam.journalism.models import BaseModel
-#from doccloud.models import Document as dcDoucment, DocumentCloudProperties
+from doccloud.models import Document as dcDoucment, DocumentCloudProperties
 
 class Document(BaseModel):
-#    document = models.ForeignKey(dcDoucment) # should this be a foreignkey?
+    document = models.ForeignKey(dcDoucment) # should this be a foreignkey?
     pass
-    
--- jaam/projects/models.py
+++ jaam/projects/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-<<<<<<< HEAD
-=======
 from jaam.journalism.models import BaseModel
->>>>>>> d47faba09693d7a2d41de4362561f291d7a221bb
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
