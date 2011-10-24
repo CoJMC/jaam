22bfd7defeb098ed6a0c1764006c8000b09ab505
Matt Claycomb <matt@semcat.net>
2011-10-24 09:44:21 -0500

    A little customization for locations and blogs admin interfaces.

--- jaam/blog/admin.py
+++ jaam/blog/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from jaam.blog.models import BlogPost, Blog
 from jaam.journalism.admin import BaseAdmin
 
+class BlogPostInline(admin.StackedInline):
+    model = BlogPost
+    extra = 1
+
 class BlogPostAdmin(BaseAdmin):
-    pass
+    list_display = ('__unicode__', 'description',)
+    list_filter = ('blog',)
 
 class BlogAdmin(BaseAdmin):
-    pass
+    list_display = ('__unicode__', 'subtitle',)
+    list_filter = ('project',)
+    inlines = [ BlogPostInline, ]
 
 admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Blog, BlogAdmin)
--- jaam/projects/admin.py
+++ jaam/projects/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from jaam.projects.models import Project, ProjectLocation
 
+class LocationInline(admin.TabularInline):
+    name = "Locations"
+    model = Project.locations.through
+    extra = 2
+    verbose_name = "Location"
+    verbose_name_plural = "Locations"
+
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    exclude = ( 'locations', )
+    inlines = [ LocationInline, ]
+    list_filter = ( 'locations', )
 
 class ProjectLocationAdmin(admin.ModelAdmin):
     pass
