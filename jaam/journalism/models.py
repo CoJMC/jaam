f5286e514aed39517b18912e27156f817380ebfe
Ryan <rking@cse.unl.edu>
2011-10-27 15:41:03 -0500

    prepping to work on Users

--- jaam/journalism/models.py
+++ jaam/journalism/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
--- jaam/profiles/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-#create models here
-class Profile(models.Model):
-    user = models.OneToOneField(User)
-    blurb = models.CharField(max_length=1000)
