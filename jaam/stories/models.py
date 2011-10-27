860b7a25ec56b4d097aba788a3463faeccca5be7
Callie Helms <calliehelms@gmail.com>
2011-10-27 14:53:12 -0500

    Gave act and stories rich text fields; registered Journalist app.

--- jaam/stories/models.py
+++ jaam/stories/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from jaam.projects.models import Project
 from jaam.journalism.models import Journalist, BaseModel
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Story(BaseModel):
     project = models.ForeignKey(Project)
     author = models.ForeignKey(Journalist)
     headline = models.CharField(max_length=200)
-    body = models.CharField(max_length=5000)
+    body = RichTextField()
     blurb = models.CharField(max_length=1000)
     class Meta:
         verbose_name = "story"
