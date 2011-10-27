2e17a543bd79830fd1ba21041f57c740b357dcf6
Callie Helms <calliehelms@gmail.com>
2011-10-27 15:39:54 -0500

    Rich textfield for bio.

--- jaam/journalism/models.py
+++ jaam/journalism/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class BaseModel(models.Model):
@@ -7,7 +8,7 @@ class BaseModel(models.Model):
 
 class Journalist(models.Model):
     user = models.OneToOneField(User, primary_key=True)
-    bio = models.TextField(null=True, blank=True)
+    bio = RichTextField(null=True, blank=True)
     major = models.CharField(max_length=255, null=True, blank=True)
     avatar = models.ImageField(upload_to='/', null = True, blank=True)
     facebookID = models.IntegerField(null=True, blank=True)
