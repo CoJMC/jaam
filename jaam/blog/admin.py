c816fda11e793c46605c88c0f9b9a7badd7fb2de
Matt Claycomb <matt@semcat.net>
2011-10-27 17:46:15 -0500

    An update to the admin interface for blog posts.

--- jaam/blog/admin.py
+++ jaam/blog/admin.py
@@ -1,19 +1,38 @@
 from django.contrib import admin
+from django import forms
 from jaam.blog.models import BlogPost, Blog
 from jaam.journalism.admin import BaseAdmin
+from django.contrib.auth.models import User
 
 class BlogPostInline(admin.StackedInline):
     model = BlogPost
     extra = 1
 
 class BlogPostAdmin(BaseAdmin):
-    list_display = ('__unicode__', 'description',)
-    list_filter = ('blog',)
+    list_display = ('__unicode__', 'description', 'author',)
+    list_filter = ('blog','author')
+    prepopulated_fields = {'slug': ('headline',)}
+    fieldsets = (
+                 (None, { 'fields': ('blog', 'headline', 'slug', 'description', 'body','tags',) },),
+                 ('Admin', { 'fields': ('author', 'published',) },),
+                )
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = []
+        if not request.user.is_superuser:
+            self.exclude.append('published')
+            self.exclude.append('author')
+        return super(BlogPostAdmin, self).get_form(request, obj, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        obj.save()
 
 class BlogAdmin(BaseAdmin):
     list_display = ('__unicode__', 'subtitle',)
     list_filter = ('project',)
-    inlines = [ BlogPostInline, ]
+    #inlines = [ BlogPostInline, ]
 
 admin.site.register(BlogPost, BlogPostAdmin)
 admin.site.register(Blog, BlogAdmin)
--- jaam/journalism/admin.py
+++ jaam/journalism/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from jaam.journalism.models import Journalist
+from jaam.journalism.models import Journalist, Tag, BaseModel
 
+class TagAdmin(admin.ModelAdmin):
+    pass
 
 class BaseAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Journalist)
+admin.site.register(Tag, TagAdmin)
