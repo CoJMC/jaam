0cbd97331ea8974063e41875ad3eaf2008f06612
Matt Claycomb <matt@semcat.net>
2011-10-28 13:50:26 -0500

    Customized stories admin interface.

--- jaam/stories/admin.py
+++ jaam/stories/admin.py
@@ -3,12 +3,26 @@ from jaam.stories.models import Story
 from jaam.journalism.admin import BaseAdmin
 
 class StoryAdmin(BaseAdmin):
+    list_display = ('__unicode__', 'blurb', 'author',)
+    list_filter = ('author',)
+    prepopulated_fields = {'slug': ('headline',)}
+
     fieldsets = (
         (None, { 'fields':
-            ('project', 'author', 'headline', 'blurb', 'body') }),
+            ('project', 'headline', 'slug', 'blurb', 'body', 'tags') }),
         ('Admin', { 'fields':
-            ('published',) }),
+            ('published', 'author') }),
     )
-    pass
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = []
+        if not request.user.is_superuser:
+            self.exclude.append('published')
+            self.exclude.append('author')
+        return super(StoryAdmin, self).get_form(request, obj, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+        obj.save()
 
 admin.site.register(Story, StoryAdmin)
