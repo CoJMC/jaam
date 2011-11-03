b0f4be880e11e80e5f452e40eab1d5f89ad8442e
Ryan <rking@cse.unl.edu>
2011-11-03 16:49:39 -0500

    Added a feedback link as well as redirecting the login dump

--- jaam/journalism/views.py
+++ jaam/journalism/views.py
@@ -8,3 +8,6 @@ from django.contrib.auth.models import User
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
     return render_to_response('user_profile.html', { 'user': user }, context_instance=RequestContext(request))
+
+def login_dump(request):
+    return render_to_response('success.html')
