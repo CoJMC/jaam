29fec4cf46311bf67cd6462e8a028e894c50a14a
Matt Claycomb <matt@semcat.net>
2011-11-28 08:20:18 -0600

    Logic for multiple layouts.

--- jaam/blog/views.py
+++ jaam/blog/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from jaam.shortcuts import render_to_response, RequestContext
 
 from jaam.blog.models import Blog, BlogPost
-
 def blog_details(request, project_slug, blog_title_slug):
     blog = get_object_or_404(Blog, slug=blog_title_slug)
     return render_to_response('blog_details.html', { 'blog': blog }, context_instance=RequestContext(request))
--- jaam/journalism/views.py
+++ jaam/journalism/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 # Create your views here.
 from django.contrib.auth.models import User
+from jaam.shortcuts import render_to_response, RequestContext
+
+def activate_layout(request, layout_name):
+    if layout_name in ['dora', 'tico']:
+        request.session['layout'] = layout_name
+        return redirect('/')
+    else:
+        return HttpResponse("Invalid layout")
 
 def user_profile(request, username):
     user = get_object_or_404(User, username=username)
