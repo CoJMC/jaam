388521df3b30f7d6c6767799b8c58cc143b00dbd
Nick Graef <nicholas.a.graef@gmail.com>
2012-01-20 11:32:53 -0600

    retab

--- jaam/api/resources.py
+++ jaam/api/resources.py
@@ -11,52 +11,52 @@ class ProjectResource(ModelResource):
         allowed_methods = ['get']
 
 class PhotoGalleryResource(ModelResource):
-	class Meta:
-		queryset = PhotoGallery.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = PhotoGallery.objects.all()
+        allowed_methods = ['get']
 
 class PhotoResource(ModelResource):
-	class Meta:
-		queryset = Photo.objects.all()
-		allowed_methods = ['get']
-		filtering = {
-			'slug': ALL,
-			'project': ALL_WITH_RELATIONS,
-			'journalist': ALL,
-		}
+    class Meta:
+        queryset = Photo.objects.all()
+        allowed_methods = ['get']
+        filtering = {
+            'slug': ALL,
+            'project': ALL_WITH_RELATIONS,
+            'journalist': ALL,
+        }
 
 class VideoGalleryResource(ModelResource):
-	class Meta:
-		queryset = VideoGallery.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = VideoGallery.objects.all()
+        allowed_methods = ['get']
 
 class VideoResource(ModelResource):
-	class Meta:
-		queryset = Video.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = Video.objects.all()
+        allowed_methods = ['get']
 
 class StoryResource(ModelResource):
-	class Meta:
-		queryset = Story.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = Story.objects.all()
+        allowed_methods = ['get']
 
 class BlogResource(ModelResource):
-	class Meta:
-		queryset = Blog.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = Blog.objects.all()
+        allowed_methods = ['get']
 
 class BlogPostResource(ModelResource):
-	class Meta:
-		queryset = BlogPost.objects.all()
-		allowed_methods = ['get']
+    class Meta:
+        queryset = BlogPost.objects.all()
+        allowed_methods = ['get']
 
 class DocumentResource(ModelResource):
-	# TODO:
-	# this one could be interesting
-	# check the TastyPie documentation on
-	# non-ORM resources, which is what this would be.
-	# 
-	# It will need to build a QuerySet of documents
-	# from DocumentCloud
-	class Meta:
-		pass
+    # TODO:
+    # this one could be interesting
+    # check the TastyPie documentation on
+    # non-ORM resources, which is what this would be.
+    # 
+    # It will need to build a QuerySet of documents
+    # from DocumentCloud
+    class Meta:
+        pass
