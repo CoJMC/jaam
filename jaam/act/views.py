6dd3729af4616ec346ec9ed071ee4d8511b86e3f
Callie Helms <calliehelms@gmail.com>
2012-02-08 16:20:30 -0600

    Basic Template of design implemented in base.html. Updated the Act app to associate with a project. Also added act view.

--- jaam/act/views.py
+++ jaam/act/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+from django.template import RequestContext
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from jaam.projects.models import Project
+
+from jaam.act.models import Act
+
+def details(request, project_slug, act_slug):
+    project = get_object_or_404(Project, slug=project_slug)
+    act = get_object_or_404(Act, slug=act_slug)
+    return render_to_response('act/act_details.html', { 'act': act }, context_instance=RequestContext(request))
--- jaam/projects/urls.py
+++ jaam/projects/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     url(r'^(?P<project_slug>[^\\]+)/videos/', include('jaam.videos.urls')),
     url(r'^(?P<project_slug>[^\\]+)/stories/', include('jaam.stories.urls')),
     url(r'^(?P<project_slug>[^\\]+)/documents/', include('jaam.document.urls')),
+    url(r'^(?P<project_slug>[^\\]+)/act/', include('jaam.act.urls')),
     url(r'^(?P<project_slug>[^\\]+)/', 'jaam.projects.views.details'),
 )
