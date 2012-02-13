1cfae08e6dbbb007199a4e878e8b45565928e815
Callie Helms <calliehelms@gmail.com>
2012-02-13 16:33:26 -0600

    Video page looking good. Still need to implement gallery here.

--- jaam/videos/views.py
+++ jaam/videos/views.py
@@ -10,12 +10,12 @@ from jaam.projects.models import Project
 def gallery_details(request, project_slug, gallery_slug):
     project = get_object_or_404(Project, slug=project_slug)
     gallery = get_object_or_404(VideoGallery, slug=gallery_slug)
-    return render_to_response('video_gallery.html', { 'gallery': gallery }, context_instance=RequestContext(request))
+    return render_to_response('videos/video_gallery.html', { 'gallery': gallery }, context_instance=RequestContext(request))
 
 def details(request, project_slug, video_id):
     project = get_object_or_404(Project, slug=project_slug)
     video = get_object_or_404(Video, pk=video_id)
-    return render_to_response('video_details.html', { 'video': video }, context_instance=RequestContext(request))
+    return render_to_response('videos/video_details.html', { 'video': video }, context_instance=RequestContext(request))
 
 def index(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
