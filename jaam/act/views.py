984647d0a01ebf7df6436cf83f4b7f5bff1310b6
Callie Helms <calliehelms@gmail.com>
2012-02-22 16:07:36 -0600

    Updated views to have the project and title.

--- jaam/act/views.py
+++ jaam/act/views.py
@@ -9,7 +9,7 @@ from jaam.act.models import Act
 def details(request, project_slug, act_slug):
     project = get_object_or_404(Project, slug=project_slug)
     act = get_object_or_404(Act, slug=act_slug)
-    return render_to_response('act/act_details.html', { 'act': act }, context_instance=RequestContext(request))
+    return render_to_response('act/act_details.html', { 'act': act, 'project': project, 'title': act}, context_instance=RequestContext(request))
 
 def information(request):
     return render_to_response('act/act_information.html', context_instance=RequestContext(request))
--- jaam/blog/views.py
+++ jaam/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from jaam.projects.models import Project
 
 from jaam.blog.models import Blog, BlogPost
 def blog_details(request, project_slug, blog_title_slug):
@@ -11,3 +12,7 @@ def blog_details(request, project_slug, blog_title_slug):
 def post_details(request, project_slug, blog_title_slug, blog_post_slug):
     post = get_object_or_404(BlogPost, slug=blog_post_slug)
     return render_to_response('blog/post_details.html', { 'post': post }, context_instance=RequestContext(request))
+
+def index(request, project_slug):
+    project = get_object_or_404(Project, slug=project_slug)
+    return render_to_response('index.html', {'project': project } ,context_instance=RequestContext(request))
