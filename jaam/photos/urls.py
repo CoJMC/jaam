f382b3fff92d066c84babd0a24f3048865fcb911
Jared Wiebelhaus <jared.wiebelhaus@gmail.com>
2012-02-21 18:38:58 -0600

    added project to photo views and added an index page for photos

--- jaam/photos/urls.py
+++ jaam/photos/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('jaam.photos.views',
     url(r'^galleries/(?P<gallery_slug>[^\\]+)/$', 'gallery_details'),
     url(r'^(?P<photo_id>\d+)/$', 'details'),
+    url(r'^$', 'index'),
 )
--- jaam/photos/views.py
+++ jaam/photos/views.py
@@ -9,15 +9,20 @@ from jaam.photos.models import Photo, PhotoGallery
 def galleries(request, project_slug):
     project = get_object_or_404(Project, slug=project_slug)
     galleries = project.photogallery_set.all()
-    return render_to_response('photos/photo_galleries.html', { 'galleries': galleries }, context_instance=RequestContext(request))
+    return render_to_response('photos/photo_galleries.html', { 'galleries': galleries, 'project': project }, context_instance=RequestContext(request))
 
 def gallery_details(request, project_slug, gallery_slug):
     project = get_object_or_404(Project, slug=project_slug)
     gallery = get_object_or_404(PhotoGallery, slug=gallery_slug)
     photos = [i.photo for i in gallery.photogalleryitem_set.order_by('order')]
-    return render_to_response('photos/photo_gallery.html', { 'gallery': gallery, 'photos': photos }, context_instance=RequestContext(request))
+    return render_to_response('photos/photo_gallery.html', { 'gallery': gallery, 'photos': photos, 'project': project }, context_instance=RequestContext(request))
 
 def details(request, project_slug, photo_id):
     project = get_object_or_404(Project, slug=project_slug)
     photo = get_object_or_404(Photo, pk=photo_id)
-    return render_to_response('photos/photo_details.html', { 'photo': photo }, context_instance=RequestContext(request))
+    return render_to_response('photos/photo_details.html', { 'photo': photo, 'project': project }, context_instance=RequestContext(request))
+
+def index(request, project_slug):
+    project = get_object_or_404(Project, slug=project_slug)
+    photos = project.photo_set.all()
+    return render_to_response('photos/index.html', {'photos': photos, 'project': project }, context_instance=RequestContext(request))
