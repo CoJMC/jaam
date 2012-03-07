7236ea493d26f0dfe6e68ea769edd4cc4d1935f1
Cole Mickens <cole.mickens@gmail.com>
2012-03-07 15:02:55 -0600

    add custom error pages (blank, should fix), modified urls/models/settings because that's cool, yo

--- jaam/settings.py
+++ jaam/settings.py
@@ -106,6 +106,13 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'jaam.urls'
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+)
+
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
     os.path.join(PROJECT_ROOT, "static_media"),
