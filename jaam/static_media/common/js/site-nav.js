9995f4deae61e8bbd97211963c0a550e06abd383
Cole Mickens <cole.mickens@gmail.com>
2012-04-19 23:53:43 -0500

    fix login/share boxes

--- jaam/static_media/common/js/site-nav.js
+++ jaam/static_media/common/js/site-nav.js
@@ -1,21 +1,10 @@
 $(document).ready(function() {
         $("#fancybox-outer").css("background", "#262626");
         $("#login-link").fancybox({
-            /*
-            'padding'   : 2, 
-            'width'     : 250,
-            'height'    : 200,
-            'autoDimensions'    : false,
-            */
+            'padding'   : 20,
         });
         $("#share-link").fancybox({
-            /*
-            'padding'   : 2,
-            'scrolling' : false,
-            'width'     : 150,
-            'height'    : 100,
-            'autoDimensions'    : false,
-            */
+            'padding'   : 20,
         });
         $("#openid-login").fancybox();
 });
