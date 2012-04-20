ab7913464a6590b6948c27a97c47b9c98c93e49a
Cole Mickens <cole.mickens@gmail.com>
2012-04-20 00:10:24 -0500

    another attempt at fixing the share btn, change styling for fancyboxes

--- jaam/static_media/common/js/site-nav.js
+++ jaam/static_media/common/js/site-nav.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
         $("#fancybox-outer").css("background", "#262626");
         $("#login-link").fancybox({
-            'padding'   : 20,
+            'padding'   : 2,
         });
         $("#share-link").fancybox({
-            'padding'   : 20,
+            'padding'   : 2,
         });
         $("#openid-login").fancybox();
 });
