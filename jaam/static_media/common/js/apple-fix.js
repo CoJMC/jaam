957ac107ff039a1e1025df6b4d327b6c3a67d96f
Cole Mickens <cole.mickens@gmail.com>
2012-04-19 19:09:56 -0500

    fix apple, fix splash

---
+++ jaam/static_media/common/js/apple-fix.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+// rewrite with jquery
+// add popup to ask them to add it to their homepage
+var iWebkit;
+if(!iWebkit){
+	iWebkit=window.onload=function(){
+		function fullscreen(){
+			var a=document.getElementsByTagName("a");
+			for(var i=0;i<a.length;i++){
+				if(a[i].className.match("noeffect")){
+
+				}
+				else{
+					a[i].onclick=function(){
+						window.location=this.getAttribute("href");
+						return false
+					}
+				}
+			}
+		}
+		function hideURLbar(){
+			window.scrollTo(0,0.9);
+		}
+		iWebkit.init=function(){
+			fullscreen();
+			hideURLbar()
+		};
+		iWebkit.init();
+	}
+}
+</script>
--- jaam/static_media/common/js/splash.js
+++ jaam/static_media/common/js/splash.js
@@ -1,42 +1,38 @@
+function runSplash() {
+    var lastDate=getCookie("GlobalEyewitness");
+    if (lastDate==null || lastDate=="") {
 
+        $("#footer").hide();
+        $("#container").hide();
+        
+        $("#JaamIntro").show();
+
+        var lastDate = "JournalismMeans";
+        setCookie("GlobalEyewitness",lastDate,null);
+        
+        $("#JaamIntro").delay(1000).fadeOut("slow");
+        $("#container").delay(1200).fadeIn("slow");
+        $("#footer").delay(1400).fadeIn("slow");
+    }
+}
 
 function setCookie(c_name,value,exdays)
 {
-var exdate=new Date();
-exdate.setDate(exdate.getDate() + exdays);
-var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-document.cookie=c_name + "=" + c_value;
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    document.cookie=c_name + "=" + c_value;
 }
 
-function getCookie(c_name)
-{
-var i,x,y,ARRcookies=document.cookie.split(";");
-for (i=0;i<ARRcookies.length;i++)
-{
-  x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-  y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-  x=x.replace(/^\s+|\s+$/g,"");
-  if (x==c_name)
-    {
-    return unescape(y);
+function getCookie(c_name) {
+    var i, x, y
+    var ARRcookies=document.cookie.split(";");
+    for (i=0;i<ARRcookies.length;i++) {
+        x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+        x = x.replace(/^\s+|\s+$/g,"");
+        if (x==c_name) {
+            return unescape(y);
+        }
     }
-  }
 }
-
-function showIntro()
-{
-  $("#JaamIntro").show();
-}
-
-function checkCookie()
-{
-var lastDate=getCookie("GlobalEyewitness");
-  if (lastDate==null || lastDate=="") 
-  {
-  	showIntro();
-  	var lastDate = "JournalismMeans";
-    setCookie("GlobalEyewitness",lastDate,null);
-  }
-}
-
-
