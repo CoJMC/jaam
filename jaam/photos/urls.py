c41d9c59fcc318d7715baa8cd4857925cb7ba967
Callie Helms <calliehelms@gmail.com>
2012-04-23 15:53:11 -0500

    matched photos to correct exif data.

--- jaam/photos/urls.py
+++ jaam/photos/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('jaam.photos.views',
     url(r'^galleries/(?P<gallery_slug>[^\\]+)/(?P<start_number>[^\\]+)/$', 'gallery_details', name="start_photo_gallery"),
     url(r'^galleries/(?P<gallery_slug>[^\\]+)/$', 'gallery_details'),
     url(r'^(?P<photo_id>\d+)/$', 'details'),
+    url(r'^exif/(?P<photo_id>\d+)/$', 'exif'),
     url(r'^$', 'galleries'),
 )
