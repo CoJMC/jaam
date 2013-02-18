49c2ecd3421fc8a3810a6f54c075425f33a13059
Cole Mickens <cole.mickens@gmail.com>
2013-02-18 15:56:40 -0600

    check on DJANGO_SETTINGS_MODULE envvar

--- manage.py
+++ manage.py
@@ -20,6 +20,8 @@ import os, sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jaam.settings")
 
+    print os.environ["DJANGO_SETTINGS_MODULE"]
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
