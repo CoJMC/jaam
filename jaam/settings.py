195d620b9e9da8ad7e3c9087fd24a7144ce8575e
Cole Mickens <cole.mickens@gmail.com>
2013-02-18 16:43:12 -0600

    fix wsgi.py

--- jaam/settings.py
+++ jaam/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'tmp/database.db', # Or path to database file if using sqlite3.
+        'NAME': 'tmp_local_jaam.db', # Or path to database file if using sqlite3.
         'USER': '', # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3.
         'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
@@ -87,7 +87,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-#SECRET_KEY = 'null'
+SECRET_KEY = 'null'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
